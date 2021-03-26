64128adf6d465753fd10c214c4d699f837366d57
Alexhb02 <Alexanderhb02@gmail.com>
2021-03-25 23:29:11 -0400

    Initial attempt at orderChildrenWithHeuristics

---
+++ src/edu/cwru/sepia/agent/minimax/GameState.java
@@ -0,0 +1,123 @@
+package edu.cwru.sepia.agent.minimax;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.action.ActionType;
+import edu.cwru.sepia.action.DirectedAction;
+import edu.cwru.sepia.action.TargetedAction;
+import edu.cwru.sepia.environment.model.state.State;
+import edu.cwru.sepia.environment.model.state.Unit;
+import edu.cwru.sepia.util.Direction;
+
+import java.util.*;
+
+/**
+ * This class stores all of the information the agent
+ * needs to know about the state of the game. For example this
+ * might include things like footmen HP and positions.
+ *
+ * Add any information or methods you would like to this class,
+ * but do not delete or change the signatures of the provided methods.
+ */
+public class GameState {
+
+    /**
+     * You will implement this constructor. It will
+     * extract all of the needed state information from the built in
+     * SEPIA state view.
+     *
+     * You may find the following state methods useful:
+     *
+     * state.getXExtent() and state.getYExtent(): get the map dimensions
+     * state.getAllResourceIDs(): returns the IDs of all of the obstacles in the map
+     * state.getResourceNode(int resourceID): Return a ResourceView for the given ID
+     *
+     * For a given ResourceView you can query the position using
+     * resource.getXPosition() and resource.getYPosition()
+     * 
+     * You can get a list of all the units belonging to a player with the following command:
+     * state.getUnitIds(int playerNum): gives a list of all unit IDs belonging to the player.
+     * You control player 0, the enemy controls player 1.
+     * 
+     * In order to see information about a specific unit, you must first get the UnitView
+     * corresponding to that unit.
+     * state.getUnit(int id): gives the UnitView for a specific unit
+     * 
+     * With a UnitView you can find information about a given unit
+     * unitView.getXPosition() and unitView.getYPosition(): get the current location of this unit
+     * unitView.getHP(): get the current health of this unit
+     * 
+     * SEPIA stores information about unit types inside TemplateView objects.
+     * For a given unit type you will need to find statistics from its Template View.
+     * unitView.getTemplateView().getRange(): This gives you the attack range
+     * unitView.getTemplateView().getBasicAttack(): The amount of damage this unit type deals
+     * unitView.getTemplateView().getBaseHealth(): The initial amount of health of this unit type
+     *
+     * @param state Current state of the episode
+     */
+    public GameState(State.StateView state) {
+    }
+
+    /**
+     * You will implement this function.
+     *
+     * You should use weighted linear combination of features.
+     * The features may be primitives from the state (such as hp of a unit)
+     * or they may be higher level summaries of information from the state such
+     * as distance to a specific location. Come up with whatever features you think
+     * are useful and weight them appropriately.
+     *
+     * It is recommended that you start simple until you have your algorithm working. Then watch
+     * your agent play and try to add features that correct mistakes it makes. However, remember that
+     * your features should be as fast as possible to compute. If the features are slow then you will be
+     * able to do less plys in a turn.
+     *
+     * Add a good comment about what is in your utility and why you chose those features.
+     *
+     * @return The weighted linear combination of the features
+     */
+    public double getUtility() {
+        return 0.0;
+    }
+
+    /**
+     * You will implement this function.
+     *
+     * This will return a list of GameStateChild objects. You will generate all of the possible
+     * actions in a step and then determine the resulting game state from that action. These are your GameStateChildren.
+     * 
+     * It may be useful to be able to create a SEPIA Action. In this assignment you will
+     * deal with movement and attacking actions. There are static methods inside the Action
+     * class that allow you to create basic actions:
+     * Action.createPrimitiveAttack(int attackerID, int targetID): returns an Action where
+     * the attacker unit attacks the target unit.
+     * Action.createPrimitiveMove(int unitID, Direction dir): returns an Action where the unit
+     * moves one space in the specified direction.
+     *
+     * You may find it useful to iterate over all the different directions in SEPIA. This can
+     * be done with the following loop:
+     * for(Direction direction : Directions.values())
+     *
+     * To get the resulting position from a move in that direction you can do the following
+     * x += direction.xComponent()
+     * y += direction.yComponent()
+     * 
+     * If you wish to explicitly use a Direction you can use the Direction enum, for example
+     * Direction.NORTH or Direction.NORTHEAST.
+     * 
+     * You can check many of the properties of an Action directly:
+     * action.getType(): returns the ActionType of the action
+     * action.getUnitID(): returns the ID of the unit performing the Action
+     * 
+     * ActionType is an enum containing different types of actions. The methods given above
+     * create actions of type ActionType.PRIMITIVEATTACK and ActionType.PRIMITIVEMOVE.
+     * 
+     * For attack actions, you can check the unit that is being attacked. To do this, you
+     * must cast the Action as a TargetedAction:
+     * ((TargetedAction)action).getTargetID(): returns the ID of the unit being attacked
+     * 
+     * @return All possible actions and their associated resulting game state
+     */
+    public List<GameStateChild> getChildren() {
+        return null;
+    }
+}
---
+++ src/edu/cwru/sepia/agent/minimax/MinimaxAlphaBeta.java
@@ -0,0 +1,148 @@
+package edu.cwru.sepia.agent.minimax;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.agent.Agent;
+import edu.cwru.sepia.environment.model.history.History;
+import edu.cwru.sepia.environment.model.state.State;
+import edu.cwru.sepia.environment.model.state.Unit;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.action.ActionType;
+import edu.cwru.sepia.action.DirectedAction;
+import edu.cwru.sepia.action.TargetedAction;
+import edu.cwru.sepia.environment.model.state.State;
+import edu.cwru.sepia.environment.model.state.Unit;
+import edu.cwru.sepia.util.Direction;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+public class MinimaxAlphaBeta extends Agent {
+
+    private final int numPlys;
+
+    public MinimaxAlphaBeta(int playernum, String[] args)
+    {
+        super(playernum);
+
+        if(args.length < 1)
+        {
+            System.err.println("You must specify the number of plys");
+            System.exit(1);
+        }
+
+        numPlys = Integer.parseInt(args[0]);
+    }
+
+    @Override
+    public Map<Integer, Action> initialStep(State.StateView newstate, History.HistoryView statehistory) {
+        return middleStep(newstate, statehistory);
+    }
+
+    @Override
+    public Map<Integer, Action> middleStep(State.StateView newstate, History.HistoryView statehistory) {
+        GameStateChild bestChild = alphaBetaSearch(new GameStateChild(newstate),
+                numPlys,
+                Double.NEGATIVE_INFINITY,
+                Double.POSITIVE_INFINITY);
+
+        return bestChild.action;
+    }
+
+    @Override
+    public void terminalStep(State.StateView newstate, History.HistoryView statehistory) {
+
+    }
+
+    @Override
+    public void savePlayerData(OutputStream os) {
+
+    }
+
+    @Override
+    public void loadPlayerData(InputStream is) {
+
+    }
+
+    /**
+     * You will implement this.
+     *
+     * This is the main entry point to the alpha beta search. Refer to the slides, assignment description
+     * and book for more information.
+     *
+     * Try to keep the logic in this function as abstract as possible (i.e. move as much SEPIA specific
+     * code into other functions and methods)
+     *
+     * @param node The action and state to search from
+     * @param depth The remaining number of plys under this node
+     * @param alpha The current best value for the maximizing node from this node to the root
+     * @param beta The current best value for the minimizing node from this node to the root
+     * @return The best child of this node with updated values
+     */
+    public GameStateChild alphaBetaSearch(GameStateChild node, int depth, double alpha, double beta)
+    {
+        return node;
+    }
+
+    /**
+     * You will implement this.
+     *
+     * Given a list of children you will order them according to heuristics you make up.
+     * See the assignment description for suggestions on heuristics to use when sorting.
+     *
+     * Use this function inside of your alphaBetaSearch method.
+     *
+     * Include a good comment about what your heuristics are and why you chose them.
+     *
+     * @param children
+     * @return The list of children sorted by your heuristic.
+     * equation: (
+     */
+    public List<GameStateChild> orderChildrenWithHeuristics(List<GameStateChild> children){
+
+        PriorityQueue<GameStateChild> orderingChildren = new PriorityQueue<GameStateChild>(new Comparator<GameStateChild>() {
+            @Override
+            public int compare(GameStateChild childOne, GameStateChild childTwo) {
+                return Integer.compare(
+                        heuristic(childOne), heuristic(childTwo));
+            }
+        });
+        for(GameStateChild child : children){
+            orderingChildren.add(child);
+        }
+
+        ArrayList<GameStateChild> orderedChildren = new ArrayList<GameStateChild>(orderingChildren.size());
+        while (!orderingChildren.isEmpty()) {
+            orderedChildren.add(orderingChildren.poll());
+        }
+
+        return orderedChildren;
+    }
+
+    public int heuristic(GameStateChild child){
+        List<Integer> playerUnits = child.state.getUnitIds(0);
+        List<Integer> enemyUnits = child.state.getUnitIds(1);
+        int sumDistance = 0;
+        int enemyOneDistance = 0;
+        int enemyTwoDistance = 0;
+        if(enemyUnits.size() == 1){
+            for(Integer playerID : playerUnits){
+                sumDistance += Math.abs(child.state.getUnit(playerID).getXPosition() - child.state.getUnit(enemyUnits.get(0)).getXPosition()) + Math.abs(child.state.getUnit(playerID).getYPosition() - child.state.getUnit(enemyUnits.get(0)).getYPosition());
+            }
+        }
+        else{
+            for(Integer playerID : playerUnits) {
+                enemyOneDistance = Math.abs(child.state.getUnit(playerID).getXPosition() - child.state.getUnit(enemyUnits.get(0)).getXPosition()) + Math.abs(child.state.getUnit(playerID).getYPosition() - child.state.getUnit(enemyUnits.get(0)).getYPosition());
+                enemyTwoDistance = Math.abs(child.state.getUnit(playerID).getXPosition() - child.state.getUnit(enemyUnits.get(1)).getXPosition()) + Math.abs(child.state.getUnit(playerID).getYPosition() - child.state.getUnit(enemyUnits.get(1)).getYPosition());
+                sumDistance += Math.min(enemyTwoDistance, enemyOneDistance);
+            }
+        }
+        return sumDistance;
+    }
+
+}
